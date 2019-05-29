3b48b5096c7f23724625bc5a10b5a111f7eaaadd
Ian Luites <IanLuites@users.noreply.github.com>
2019-05-29 13:35:46 +0200

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CommonX.MixProject do
   def project do
     [
       app: :common_x,
-      version: "0.0.5",
+      version: "0.1.0",
       description: "Extension of common Elixir modules.",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
