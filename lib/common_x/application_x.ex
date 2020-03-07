f3ad6da1bdb31f075347b9032a1df3d7ed0198be
Ian Luites <IanLuites@users.noreply.github.com>
2020-03-07 13:48:36 +0100

    Add `ApplicationX.main/0`.

--- lib/common_x/application_x.ex
+++ lib/common_x/application_x.ex
@@ -7,6 +7,74 @@ defmodule ApplicationX do
   @ignore [:kernel, :stdlib, :elixir, :logger]
   @load_error 'no such file or directory'
 
+  main_app =
+    if p = Process.whereis(Mix.TasksServer) do
+      module =
+        p
+        |> Agent.get(&Map.keys/1)
+        |> Enum.find_value(false, fn
+          {:task, task, m} when task in ~W(app.start deps.loadpaths) -> m
+          _ -> false
+        end)
+
+      if module, do: module.project()[:app]
+    end
+
+  main_app =
+    if main_app do
+      main_app
+    else
+      [
+        Path.join(File.cwd!(), "../../mix.exs"),
+        Path.join(File.cwd!(), "mix.exs"),
+        Path.join(__DIR__, "../../../../mix.exs"),
+        Path.join(__DIR__, "../../mix.exs")
+      ]
+      |> Enum.find_value("", fn file ->
+        case File.read(file) do
+          {:ok, d} -> d
+          _ -> false
+        end
+      end)
+      |> (fn v ->
+            ~r/app:\W*:(?<app>[a-z\_]+)/
+            |> Regex.named_captures(v)
+            |> Kernel.||(%{})
+            |> Map.get("app", to_string(Mix.Project.config()[:app]))
+          end).()
+      |> String.to_atom()
+    end
+
+  @doc ~S"""
+  The atom of the current main application.
+
+  For example take an application called `:my_app`,
+  which includes the `:my_dep` dependencies,
+  which has `:common_x` as dependency.
+
+  So:
+  ```
+  :my_app
+  ├── ...
+  └── :my_dep
+      ├── ...
+      └── :common_x
+  ```
+
+  In that scenario calling `ApplicationX.main` will return in `:my_app`
+  both for code in `:my_app` and `:my_dep`.
+
+
+  ## Examples
+
+  ```elixir
+  iex> ApplicationX.main
+  :common_x
+  ```
+  """
+  @spec main :: atom
+  def main, do: unquote(main_app)
+
   @doc ~S"""
   List all available applications excluding system ones.
 
